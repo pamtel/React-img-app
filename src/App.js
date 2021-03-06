a05011895ba1282e235153cdee5136d360b35d4b
pamtel <pamelaakosahiante@gmail.com>
2021-03-06 13:53:39 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -2,25 +2,78 @@ import './App.css';
 import Header from "./Components/Header";
 import Search from "./Components/Search";
 import Images from "./Components/Images";
-import { useState } from 'react';
-
-const API = 'https://api.unsplash.com/photos/?client_id=yMOEW96irltYOVkTWeZvtHM56ypGXuJc6D-h9zi8sG0';
+import { useState , useEffect} from 'react';
+import axios from 'axios';
+const API = 'https://api.unsplash.com/photos/?client_id=yMOEW96irltYOVkTWeZvtHM56ypGXuJc6D-h9zi8sG0&per_page=30';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [noData, setNoData] = useState(false);
   const [photos, setPhotos] = useState([]);
 
-  const getData = (data) => { 
-    setPhotos(data);
-    // console.log('Photos', photos);
-
+  // Search for images
+  const search = (data) => {
+    console.log('Photos',photos.length)
+    // if()
+  setLoading(true);
+    if(!data){
+      setLoading(false);
+      setNoData(true);
+    }else{
+      const SearchImages = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${data}&client_id=yMOEW96irltYOVkTWeZvtHM56ypGXuJc6D-h9zi8sG0`
+      axios.get(SearchImages)
+      .then(res => {
+        setPhotos(res.data.results);
+        setLoading(false);
+      })
+      .catch(err => {
+           console.log(err);
+            setError(true)
+            setLoading(false)
+      });
+    }
   }
+
+//  Display api images
+  const display = () => {
+      axios.get(API)
+      .then(res => {
+          console.log('The photos from App.js', res.data);
+          setPhotos(res.data);
+          setLoading(false);
+      })
+      .catch(err => {
+          console.log(err);
+          setError(true)
+          setLoading(false)
+      })
+  }
+
+
+  useEffect(() => {
+    display()
+  }, []);
+
+  if(noData) {
+    return <h2 className="text-center">Ooops!!! Data not found</h2>
+  }
+  if(error) {
+      return <h2 className="text-center">Ooops!!! Something went wrong</h2>
+  }
+  
   return (
     <div>
-      <Header getData={getData}/>
+      <Header search={search}/>
       {/* <Search/> */}
-      <Images   photos={photos} />
+      <Images photos={photos} />
     </div>
   );
+
+  if(loading) {
+    return <h2 className="text-center">Loading...</h2>
+  }
+  
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,31 +3,12 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function Header(props) {
-    const {getData} = props;
-   
-    const [searchPhotos, setSearchPhotos] = useState([])
-    const [error, setError] = useState(false)
     const [input, setInput] = useState("")
 
     const handleInput = (e) => {
         setInput(e.target.value);
-        const SearchImages = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${e.target.value}&client_id=yMOEW96irltYOVkTWeZvtHM56ypGXuJc6D-h9zi8sG0`
-        axios.get(SearchImages)
-        .then(res => {
-            getData(res.data.results);
-            setSearchPhotos(res.data.results)
-        })
-        .catch(err => {
-            setError(true)
-        })
+        props.search(e.target.value);
     }
-
-    const display = () => {
-       
-    }
-    useEffect(() => {
-        display()
-    }, []) 
 
     return (
         <div className="bg-green-800 h-20 flex justify-center items-center">
